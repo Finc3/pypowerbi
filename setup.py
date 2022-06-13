20806fc6ba1f60637563cb7ca9c29c12571a3205
thms <thomas.masquart@gmail.com>
2022-06-13 20:51:16 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pypowerbi',
       version='0.27',
       description='A python library for Microsoft\'s PowerBI',
-      url='http://github.com/Finc3/pypowerbi',
+      url='http://github.com/cmberryau/pypowerbi',
       author='Chris Berry',
       author_email='chris@chrisberry.com.au',
       license='MIT',
